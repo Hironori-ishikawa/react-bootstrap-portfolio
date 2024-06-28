c4be067ac397888dc44c25a812ce2c507806b9ff
Hironori Ishikawa <>
2024-06-28 10:15:24 +0900

    fix2

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -12,29 +12,31 @@ export const Skill = () => {
           <h2 class="SkillTitle">Skill</h2>
           <div class="row text-center">
 
-            <div class="col-md-4 services">
+            <div class="col-md-3 services">
               <img src={NextJsImage} alt='NextJsアイコン' className='NextJsImage' />
               <h3 className='SkillNextJs'>Next.js</h3>
               <p className='SkillNextJsText'>Next.js学習中です</p>
 
             </div>
-            <div class="col-md-4 services">
+            <div class="col-md-3 services">
               <img src={reactImage} alt='reactアイコン' className='reactImage' />
               <h3 className='SkillReact'>React</h3>
               <p className='SkillReactText'>Reactがつかえます</p>
             </div>
 
-            <div class="col-md-4 services">
+            <div class="col-md-3 services">
+              <img src={PhpImage} alt='Phpアイコン' className='PhpImage' />
+              <h3 className='SkillPhp'>php / Laravel</h3>
+              <p className='SkillPhpText'>Laravelがつかえます</p>
+            </div>
+
+            <div class="col-md-3 services">
               <img src={HtmlCssJsImage} alt='HtmlCssJsアイコン' className='HtmlCssJsImage' />
               <h3 className='SkillHtmlCssJs'>HTML/CSS/JavaScript</h3>
               <p className='SkillHtmlCssJsText'>HTML/CSS/Jsがつかえます</p>
             </div>
 
-            <div class="col-md-4 services">
-              <img src={PhpImage} alt='Phpアイコン' className='PhpImage' />
-              <h3 className='SkillPhp'>php / Laravel</h3>
-              <p className='SkillPhpText'>Laravelがつかえます</p>
-            </div>
+
           </div>
         </div>
       </section>
