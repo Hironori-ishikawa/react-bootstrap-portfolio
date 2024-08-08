b812400e79b02b72d9737bf585ae22f241ca416c
Hironori Ishikawa <>
2024-08-08 21:32:10 +0900

    fix24/8/8-2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,10 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+
 
 export const Footer = () => {
   return (
     <div className='Footer'>
       <div className='FooterBox'>
-        <div className='FooterNav'>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <a href='https://nextjs-blog-app-one-gamma.vercel.app/' target="_blank" rel="noopener noreferrer">Blog
-                </a>
-              </li>
-              {/* <li>
-                <Link to="/sns">Sns</Link>
-              </li> */}
-            </ul>
-          </nav>
-        </div>
         <div className='FooterName'>H.I Portfolio @H.R.I</div>
 
       </div>
